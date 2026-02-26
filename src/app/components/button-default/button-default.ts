c11316321b37de6e88d53714aa187c049d49eb12
DanielCorreia10 <danieljscorreia10@gmail.com>
2026-02-26 20:42:54 -0300

    feat: uso do componente button

--- src/app/components/button-default/button-default.ts
+++ src/app/components/button-default/button-default.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button-default',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './button-default.css',
 })
 export class ButtonDefault {
-
+  
+  @Input() text: string = '';
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
+import { ButtonDefault } from "../../components/button-default/button-default";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Header],
+  imports: [Header, ButtonDefault],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
